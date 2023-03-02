8e779e5951d7bab4c3d234c45a2ee7da398930a4
Deva <103584094+MrDevaa@users.noreply.github.com>
2023-03-03 00:49:17 +0100

    use Readfile method

---
+++ Readfile.js
@@ -0,0 +1,22 @@
+let http = require("http");
+let fs = require("fs");
+
+let server = http.createServer();
+server.on("request", (request, reponse) => {
+  fs.readFile("index.html", (err, data) => {
+    if (err) {
+      reponse.writeHead(404);
+
+      reponse.end("ce fichier exisite pas");
+    } else {
+      reponse.writeHead(200, {
+        "Content-type": "text/html; charset=utf-8",
+      });
+
+      reponse.end("data");
+    }
+  });
+});
+server.listen(5050);
+
+//problème avec la Data
