6d371dbf4a35f3d10ee53c56f020a168d646e305
Deva <103584094+MrDevaa@users.noreply.github.com>
2023-03-03 00:45:49 +0100

    first method 1

---
+++ urlMethod1.js
@@ -0,0 +1,20 @@
+let http = require("http");
+let fs = require("fs");
+let url = require("url");
+
+let server = http.createServer();
+
+server.on("request", (request, response) => {
+  response.writeHead(200);
+
+  let query = url.parse(request.url, true).query;
+
+  if (query.name === undefined) {
+    response.write("Bonjour Anonyme");
+  } else {
+    response.write("Bonjour" + query.name);
+  }
+
+  response.end();
+});
+server.listen(5050);
